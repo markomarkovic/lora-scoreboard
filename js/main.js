711e87cad5dd8fb1bfbcad6118c6b0f5f8e6cee4
Marko Marković <okram@civokram.com>
2013-03-27 23:54:24 +0100

    Player names editing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 
+$(function() { // document.ready
+
+	// Edit player names
+	$('table')
+		.on('click', 'div.player', function() {
+			var el = $(this);
+			var th = el.parent();
+			var val = el.html();
+
+			th.html(
+				$('<input>')
+					.addClass('player')
+					.val(val)
+			);
+			th.find('input').focus();
+		})
+		.on('blur', 'input.player', function() {
+			var el = $(this);
+			var th = el.parent();
+			var val = el.val();
+
+			th.html(
+				$('<div>')
+					.addClass('player')
+					.html(val)
+			);
+		});
+
+});
